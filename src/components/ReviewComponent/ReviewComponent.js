8abb78f020057170f5edd39e40e5d25cfdf8c31c
Elliot643 <elliot643@hotmail.com>
2019-10-16 15:45:23 +0200

    Added mapcomponent and made changes to reviewcomponent to show a map

--- src/components/ReviewComponent/ReviewComponent.js
+++ src/components/ReviewComponent/ReviewComponent.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './ReviewComponent.css';
+import MapComponent from "../MapComponent/MapComponent"
 
 
 const ReviewComponent = (props) => {
@@ -9,6 +10,7 @@ const ReviewComponent = (props) => {
         <div className='reviewContainer'>
             <div className='reviewBody'>
                 <h1>This is the review Component</h1>
+                <h2><MapComponent address="Klostergatan 6, Växjö"/></h2>
             </div>
         </div>
              
